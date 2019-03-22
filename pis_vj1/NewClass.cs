2b669ee8515827ebfe754fcb5dcc4769bdcf0870
bkovac00 <bkovac00@fesb.hr>
2019-03-22 14:55:20 +0100

    bla

--- pis_vj1/NewClass.cs
+++ pis_vj1/NewClass.cs
@@ -1,30 +1,17 @@
-<<<<<<< HEAD
+
 ﻿using System;
-=======
-using System;
->>>>>>> 52b8d498f6313e1e2e92522d59ddb24eb5773ee9
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-namespace pis_vj1
-{
-    class NewClass
-    {
 
-=======
-namespace pis_v1
-=======
 namespace pis_vj1
->>>>>>> 717eb41a494d5de50473574790908b46054083e8
 {
     class NewClass
     {
         public void bok()
         { }
->>>>>>> 52b8d498f6313e1e2e92522d59ddb24eb5773ee9
+
     }
 }
